039139672c25112ab034b2f2433a510d3dcd09ab
David Justice <david@devigned.com>
2016-11-03 11:54:58 -0700

    deal with CE and full intellij

--- fluent/setup.sh
+++ fluent/setup.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
 ./teardown.sh
-open -a 'IntelliJ IDEA' compute-java-manage-vm/
+if [ ! -e '/Applicaitons/IntelliJ IDEA.app' ]; then
+  open -a 'IntelliJ IDEA' compute-java-manage-vm/
+else
+  open -a 'IntelliJ IDEA CE' compute-java-manage-vm/
+fi
